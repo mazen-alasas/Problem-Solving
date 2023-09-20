3b46a1aca89d2c4ed6b6a7a576989abf1f2d02e1
Mazen Ahmed Alasas <112027614+mazen-alasas@users.noreply.github.com>
2023-09-20 11:48:35 +0300

    Add files via upload

---
+++ CodeForces/Groups/Assiut University Training - Newcomers/Sheet #1 (Data type - Conditions)/T - Sort Numbers.cpp
@@ -0,0 +1,35 @@
+///               بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ
+/** إن الله وملائكته يصلون على النبي يا أيها الذين آمنوا صلوا عليه وسلموا تسليما **/
+#include <bits/stdc++.h>
+#define  ll         long long
+#define  fix(n)     fixed << setprecision(n)
+#define  T          ll tests; cin >> tests; while(tests--)
+#define  IOS        ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+const int mod = 1e9 + 7;
+using namespace std;
+void Online() {
+	#ifdef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int main() {
+	IOS
+	// Online();
+	ll a, b, c; cin >> a >> b >> c;
+	ll s = a + b + c;
+	cout << min({a, b, c}) << endl;
+	cout << s - max({a, b, c}) - min({a, b, c}) << endl;
+	cout << max({a, b, c}) << endl << endl;
+	cout << a << endl << b << endl << c;
+	return 0;
+}
+
+
+
+
+
+
+
+
