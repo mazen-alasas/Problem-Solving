47fdfe86860e71a964e2af87b9a9a4c3e2d3e4a5
Mazen Ahmed Alasas <112027614+mazen-alasas@users.noreply.github.com>
2023-11-06 18:27:16 +0200

    add a, b

---
+++ CodeForces/Groups/ICPC Assiut University Training - Juniors Phase 1 Sheets/Juniors Phase 1 Practice #3 ( STL 2 )/A - Registration System.cpp
@@ -0,0 +1,33 @@
+///              بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ
+/** إن الله وملائكته يصلون على النبي يا أيها الذين آمنوا صلوا عليه وسلموا تسليما **/
+#include <bits/stdc++.h>
+#define  ll         long long
+#define  fix(n)     setprecision(n)
+#define  T          ll tests; cin >> tests; while(tests--)
+#define  IOS        ios_base::sync_with_stdio(0); cin.tie(0);
+const int mod = 1e9 + 7, N = 1e5 + 9, INF = 0x3f3f3f3f;
+const double eps = 1e-9;
+using namespace std;
+void Online() {
+	#ifdef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int main() {
+	IOS
+	//Online();
+	ll n; cin >> n;
+	unordered_map<string, ll> mp;
+	for(ll i = 0; i < n; i++){
+        string s; cin >> s;
+        if(!mp[s]) {
+            cout << "OK\n";
+        } else {
+            cout << s << mp[s] << endl;
+        }
+        mp[s]++;
+	}
+ 	return 0;
+}
---
+++ CodeForces/Groups/ICPC Assiut University Training - Juniors Phase 1 Sheets/Juniors Phase 1 Practice #3 ( STL 2 )/B- Less or Equal.cpp
@@ -0,0 +1,31 @@
+///              بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ
+/** إن الله وملائكته يصلون على النبي يا أيها الذين آمنوا صلوا عليه وسلموا تسليما **/
+#include <bits/stdc++.h>
+#define  ll         long long
+#define  fix(n)     setprecision(n)
+#define  T          ll tests; cin >> tests; while(tests--)
+#define  IOS        ios_base::sync_with_stdio(0); cin.tie(0);
+const int mod = 1e9 + 7, N = 1e5 + 9, INF = 0x3f3f3f3f;
+const double eps = 1e-9;
+using namespace std;
+void Online() {
+	#ifdef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int main() {
+	IOS
+	//Online();
+	ll n, k; cin >> n >> k;
+	ll a[n];
+	for(ll i = 0; i < n; cin >> a[i++]);
+	sort(a, a + n);
+	if(!k) {
+		cout << (a[0] == 1 ? -1 : 1);
+	} else {
+		cout << (k < n && a[k] == a[k - 1] ? -1 : a[k - 1]);
+	}
+ 	return 0;
+}
