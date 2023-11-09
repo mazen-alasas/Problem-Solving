27804ee925ad196bf6d4758e6b0a1729adc186bd
Mazen Ahmed Alasas <112027614+mazen-alasas@users.noreply.github.com>
2023-11-09 15:54:58 +0200

    add a, b, e

---
+++ CodeForces/Groups/ICPC Assiut University Training - Juniors Phase 1 Sheets/Juniors Phase 1 Practice #4 ( Binary search , Two pointers )/E - Game.cpp
@@ -0,0 +1,30 @@
+///              بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ
+/** إن الله وملائكته يصلون على النبي يا أيها الذين آمنوا صلوا عليه وسلموا تسليما **/
+#include <bits/stdc++.h>
+#define  ll         long long
+#define  fix(n)     setprecision(n)
+#define  T          ll tests; cin >> tests; while(tests--)
+#define  IOS        ios_base::sync_with_stdio(0); cin.tie(0);
+const int mod = 1e9 + 7, N = 1e5 + 9, INF = 0x3f3f3f3f;
+const double eps = 1e-9;
+using namespace std;
+void Online() {
+	#ifdef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int main() {
+	IOS
+	//Online();
+	ll n, q; cin >> n >> q;
+	ll a[n];
+	for(ll i = 0; i < n; cin >> a[i++]);
+	while(q--) {
+		ll k; cin >> k;
+		ll idx = upper_bound(a, a + n, k) - a;
+		cout << (idx < n ? a[idx] : -1) << endl;
+	}
+ 	return 0;
+}
