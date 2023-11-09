3f3a2f005b6a93b7c910b51f7e424540a6ed22f8
Mazen Ahmed Alasas <112027614+mazen-alasas@users.noreply.github.com>
2023-11-09 15:55:44 +0200

    add bb

---
+++ CodeForces/Groups/ICPC Assiut University Training - Juniors Phase 1 Sheets/Juniors Phase 1 Practice #4 ( Binary search , Two pointers )/B - Balanced Team.cpp
@@ -0,0 +1,31 @@
+///              بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ
+/** إن الله وملائكته يصلون على النبي يا أيها الذين آمنوا صلوا عليه وسلموا تسليما **/
+#include <bits/stdc++.h>
+#define  ll         long long
+#define  fix(n)     setprecision(n)
+#define  T          ll tests; cin >> tests; while(tests--)
+#define  IOS        ios_base::sync_with_stdio(0); cin.tie(0);
+const int mod = 1e9 + 7, N = 1e5 + 9, INF = 0x3f3f3f3f;
+const double eps = 1e-9;
+using namespace std;
+void Online() {
+	#ifdef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int main() {
+	IOS
+	//Online();
+	ll n; cin >> n;
+	ll a[n], mx = 0;
+	for(ll i = 0; i < n; cin >> a[i++]);
+	sort(a, a + n);
+	for(ll i = 0; i < n; i++) {
+		ll x = lower_bound(a, a + n, a[i] - 5) - a;
+		mx = max(mx, i - x + 1);
+	}
+	cout << mx;
+ 	return 0;
+}
