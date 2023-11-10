eb823b9f65b6d955536ed44a7cf9cc01875a849f
Mazen Ahmed Alasas <112027614+mazen-alasas@users.noreply.github.com>
2023-11-10 14:32:43 +0200

    add o, d, g sol

---
+++ CodeForces/Groups/ICPC Assiut University Training - Juniors Phase 1 Sheets/Juniors Phase 1 Practice #3 ( STL 2 )/G - Chat Order.cpp
@@ -0,0 +1,33 @@
+///              بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ
+/** إن الله وملائكته يصلون على النبي يا أيها الذين آمنوا صلوا عليه وسلموا تسليما **/
+#include <bits/stdc++.h>
+#define  ll         long long
+#define  fix(n)     setprecision(n)
+#define  T          ll tests; cin >> tests; while(tests--)
+#define  IOS        ios_base::sync_with_stdio(0); cin.tie(0);
+const int mod = 1e9 + 7, N = 1e5 + 9, INF = 0x3f3f3f3f;
+const double eps = 1e-9;
+using namespace std;
+void Online() {
+	#ifdef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int main() {
+	IOS
+	//Online();
+	ll n; cin >> n;
+	string s[n];
+	for(ll i = 0; i < n; cin >> s[i++]);
+	reverse(s, s + n);
+	unordered_set<string> st;
+	for(ll i = 0; i < n; i++) {
+		if(!st.count(s[i])) {
+			st.insert(s[i]);
+			cout << s[i] << endl;
+		}
+	}
+ 	return 0;
+}
