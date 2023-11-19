54f05889771c79f7ab65b63d07bf0d94a9e0b8aa
Mazen Ahmed Alasas <112027614+mazen-alasas@users.noreply.github.com>
2023-11-19 17:49:49 +0200

    add f, t

---
+++ CodeForces/Groups/ICPC Assiut University Training - Juniors Phase 1 Sheets/Juniors Phase 1 Practice #3 ( STL 2 )/T - Ugly Number.cpp
@@ -0,0 +1,33 @@
+///              بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ
+/** إن الله وملائكته يصلون على النبي يا أيها الذين آمنوا صلوا عليه وسلموا تسليما **/
+#include <bits/stdc++.h>
+#define  ll         long long
+#define  fix(n)     setprecision(n)
+#define  T          ll tests; cin >> tests; while(tests--)
+#define  IOS        ios_base::sync_with_stdio(0); cin.tie(0);
+const int mod = 1e9 + 7, N = 1e5 + 9, INF = 0x3f3f3f3f;
+const double eps = 1e-9;
+using namespace std;
+void Online() {
+	#ifdef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int main() {
+	IOS
+	//Online();
+	ll k; cin >> k;
+	vector<ll> ugly(k, 1);
+	ll p2 = 0, p3 = 0, p5 = 0;
+	for(ll i = 1; i < k; ++i) {
+		ll nxt = min({ugly[p2] * 2, ugly[p3] * 3, ugly[p5] * 5});
+		ugly[i] = nxt;
+		p2 += (nxt == ugly[p2] * 2);
+		p3 += (nxt == ugly[p3] * 3);
+		p5 += (nxt == ugly[p5] * 5);
+	}
+	cout << ugly[k - 1];
+	return 0;
+}
