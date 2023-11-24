2745efa819d769210462f8f3a35ad1497bb1e227
Mazen Ahmed Alasas <112027614+mazen-alasas@users.noreply.github.com>
2023-11-24 19:52:44 +0200

    add c

---
+++ CodeForces/Groups/ICPC Assiut University Training - Standard - Juniors Phase 1/Standard #4 (Binary search , two pointers )/C - Solve Equation.cpp
@@ -0,0 +1,33 @@
+///              بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ
+/** إن الله وملائكته يصلون على النبي يا أيها الذين آمنوا صلوا عليه وسلموا تسليما **/
+#include <bits/stdc++.h>
+#define  ll         long long
+#define  fix(n)     fixed << setprecision(n)
+#define  T          ll tests; cin >> tests; while(tests--)
+#define  IOS        ios_base::sync_with_stdio(0); cin.tie(0);
+const int mod = 1e9 + 7, N = 1e5 + 9, INF = 0x3f3f3f3f;
+const double eps = 1e-6;
+using namespace std;
+void Online() {
+	#ifdef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int main() {
+	IOS
+	//Online();
+	long double c, t; cin >> c >> t;
+	long double l = 0, r = 100000000;
+	while(l <= r) {
+		long double mid = (l + r) / 2;
+		long double ti = c * mid * log2(mid);
+		if(ti <= t)
+			l = mid + eps;
+		else
+			r = mid - eps;
+	}
+	cout << fix(6) << l;
+	return 0;
+}
