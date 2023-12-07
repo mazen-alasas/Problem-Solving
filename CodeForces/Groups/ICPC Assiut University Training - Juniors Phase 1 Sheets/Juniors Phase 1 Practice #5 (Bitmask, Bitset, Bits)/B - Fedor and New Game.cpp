7a011d0a1d4b2a2491760d002b7a773664dcf4ed
Mazen Ahmed Alasas <112027614+mazen-alasas@users.noreply.github.com>
2023-12-07 09:45:22 +0200

    add a,b,c

---
+++ CodeForces/Groups/ICPC Assiut University Training - Juniors Phase 1 Sheets/Juniors Phase 1 Practice #5 (Bitmask, Bitset, Bits)/B - Fedor and New Game.cpp
@@ -0,0 +1,30 @@
+///              بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ
+/** إن الله وملائكته يصلون على النبي يا أيها الذين آمنوا صلوا عليه وسلموا تسليما **/
+#include <bits/stdc++.h>
+#define  ll         long long
+#define  fix(n)     setprecision(n)
+#define  T          ll tests; cin >> tests; while(tests--)
+#define  IOS        ios_base::sync_with_stdio(0); cin.tie(0);
+const int mod = 1e9 + 7, N = 1e5 + 9, INF = 0x3f3f3f3f;
+const double eps = 1e-9;
+using namespace std;
+void Online() {
+	#ifdef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int main() {
+	IOS
+	//Online();
+	ll n, m, k; cin >> n >> m >> k;
+	ll a[m + 1];
+	for(ll i = 0; i < m + 1; cin >> a[i++]);
+	ll ctr = 0;
+	for(ll i = 0; i < m; i++) {
+		ctr += (__builtin_popcount(a[m] ^ a[i]) <= k);
+	}
+	cout << ctr;
+	return 0;
+}
