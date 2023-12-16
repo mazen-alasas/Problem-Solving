b6b367fe4ef1ca523a0a4bfb40646a1a50efd3bd
Mazen Ahmed Alasas <112027614+mazen-alasas@users.noreply.github.com>
2023-12-16 17:28:59 +0200

    add 591A

---
+++ CodeForces/Random in C#/591A - Wizards' Duel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace app1 {
+    internal class Program {
+        static void Main(string[] args) {
+            double l = double.Parse(Console.ReadLine());
+            double p = double.Parse(Console.ReadLine());
+            double q = double.Parse(Console.ReadLine());
+            Console.WriteLine((l / (p + q)) * p);
+            
+        }
+    }
+}
